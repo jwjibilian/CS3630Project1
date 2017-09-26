2a79dd559fe49632fefd5cd4cb869b86e1516822
Joshua <jwjibilian@gmail.com>
2017-09-25 21:32:18 -0400

    Liveimage thingy

--- CS3630_Lab2/go_to_cube/livefilter.py
+++ CS3630_Lab2/go_to_cube/livefilter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 #!c:/Python35/python3.exe -u
+
 import asyncio
 import sys
 import cv2
@@ -9,25 +10,42 @@ import time
 import os
 
 from cozmo.util import degrees, distance_mm
-
-from glob import glob
-
-from find_cube import *
-
 try:
-    from PIL import ImageDraw, ImageFont
+    # for Python2
+    from Tkinter import *   ## notice capitalized T in Tkinter
 except ImportError:
-    sys.exit('run `pip3 install --user Pillow numpy` to run this example')
-def nothing(x):
-    pass
+    # for Python3
+    from tkinter import *
 
-YELLOW_LOWER = np.array([9, 115, 151])
-YELLOW_UPPER = np.array([179, 215, 255])
+mainWindow = Tk()
 
-GREEN_LOWER = np.array([0,0,0])
-GREEN_UPPER = np.array([179, 255, 60])
+Label(mainWindow, text="Gain").grid(row=0)
+gain = Scale(mainWindow, from_=0, to=500,orient=HORIZONTAL)
+gain.grid(row=0,column=1)
 
-# Define a decorator as a subclass of Annotator; displays the keypoint
+Label(mainWindow, text="Exposure").grid(row=1)
+exposure = Scale(mainWindow, from_=0.1, to=4.0, orient=HORIZONTAL,resolution = 0.01)
+exposure.grid(row=1, column = 1)
+
+Label(mainWindow, text="Hue").grid(row=2)
+hue = Scale(mainWindow, from_=0, to=255, orient=HORIZONTAL)
+hue.grid(row=2, column = 1)
+
+Label(mainWindow, text="Saturation").grid(row=3)
+saturation = Scale(mainWindow, from_=0, to=255, orient=HORIZONTAL)
+saturation.grid(row=3, column = 1)
+
+Label(mainWindow, text="Value").grid(row=4)
+value = Scale(mainWindow, from_=0, to=255, orient=HORIZONTAL)
+value.grid(row=4, column = 1)
+
+
+
+
+
+
+mainloop()
+
 class BoxAnnotator(cozmo.annotate.Annotator):
 
     cube = None
@@ -51,7 +69,6 @@ class BoxAnnotator(cozmo.annotate.Annotator):
             BoxAnnotator.cube = None
 
 
-
 async def run(robot: cozmo.robot.Robot):
 
     robot.world.image_annotator.annotation_enabled = False
@@ -61,11 +78,14 @@ async def run(robot: cozmo.robot.Robot):
     robot.camera.color_image_enabled = True
     robot.camera.enable_auto_exposure = True
 
-    gain,exposure,mode = 390,3,1
+
 
     try:
 
         while True:
+            gain, exposure, mode = 390, 3, 1
+
+
             event = await robot.world.wait_for(cozmo.camera.EvtNewRawCameraImage, timeout=30)   #get camera image
             if event.image is not None:
                 image = cv2.cvtColor(np.asarray(event.image), cv2.COLOR_BGR2RGB)
@@ -108,7 +128,6 @@ async def run(robot: cozmo.robot.Robot):
     except cozmo.RobotBusy as e:
         print(e)
     #cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     cozmo.run_program(run, use_viewer = True, force_viewer_on_top = True)
